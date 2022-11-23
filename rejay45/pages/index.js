f3fc63baf71677f9fb6ff60dfbc4a9c72f9cd8c6
CyrineOsorio <cyrineosorio18@gmail.com>
2022-11-24 07:08:43 +0800

    Shadow

--- rejay45/pages/index.js
+++ rejay45/pages/index.js
@@ -3,7 +3,6 @@ import AboutUs from "../components/AboutUs";
 import Hero from '../components/Hero'
 import Navbar from "../components/Navbar";
 import Services from "../components/Services";
-import dulo from "../components/Dulo";
 import Dulo from "../components/Dulo";
 
 
